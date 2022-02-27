b75ce2625f06c28905ac112e9407ad34e0e39921
Rohit Garg <43957599+rohitgarg200@users.noreply.github.com>
2022-02-27 23:39:16 +0530

    Update Count Primes.cpp

--- Math/Count Primes.cpp
+++ Math/Count Primes.cpp
@@ -3,22 +3,24 @@ public:
     int countPrimes(int n) {
   
         if(n<=1){
-        return 0;
+            return 0;
         }
-
-        vector<int>check(n,0);
+//create an array to store number status
+        vector<bool>check(n,0);
         
         n=n-1;
-        for(int i=3;i<=n/3;i+=2){
-        for(int j=3;j<=n/i;j+=2){
-            check[i*j]=1;
+        // here check only odd numbers 
+        for(int i=3;i<=sqrt(n);i+=2){
+            for(int j=3;j<=n/i;j+=2){
+              check[i*j]=1;
         }
     }
     int ans=n>=2?1:0;
+        //finding prime numbers
         for(int i=3;i<=n;i+=2){
-        if(!check[i]){
-            ans++;
-        }    
+            if(!check[i]){
+                ans++;
+            }    
         }
         return ans;
         
